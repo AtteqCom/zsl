fa803810dfd49506f84c80cb7b34c93608cdf13f
babky <devnull@localhost>
2013-01-22 10:54:43 +0100

    Pridany redis. #6019

--- service/sportky/tasks/test/db_test_task.py
+++ service/sportky/tasks/test/db_test_task.py
@@ -8,18 +8,18 @@ from injector import inject
 import sqlalchemy.engine
 from application import service_application
 
-'''
-Connects to a database and executes a simple query. The result of the query should be 6.
-
-<emph>Input</emph>
-No input.
-
-<emph>Output</emph>
-Returns just a number 6.
-
-@author: Martin Babka
-'''
 class DbTestTask(object):
+    '''
+    Connects to a database and executes a simple query. The result of the query should be 6.
+
+    <emph>Input</emph>
+    No input.
+
+    <emph>Output</emph>
+    Returns just a number 6.
+
+    @author: Martin Babka
+    '''
 
     @inject(db=sqlalchemy.engine.Engine)
     def __init__(self, db):
