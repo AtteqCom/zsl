756d76cb530aca135520d4f011c6496c7dd2a1a2
babka <devnull@localhost>
2013-03-27 18:50:44 +0100

    Odobrane sportky veci.

--- service/db/generate.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-sqlautocode mysql://robot_wr:robot_wr@localhost/sportkynew -d -o models/raw/models.py -t state,sport,sport_club,sport_club_field,image,gallery,gallery_image,resource,video_daily
--- service/sportky/tasks/test/simple_task.py
+++
@@ -1,8 +0,0 @@
-from task.task_decorator import json_input, json_output
-
-class SimpleTask:
-
-    @json_input
-    @json_output
-    def perform(self, data):
-        return ["Simple task with data '{0}' of type {1}.".format(data.get_data(), type(data.get_data()))]
