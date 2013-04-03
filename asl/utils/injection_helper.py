e7c2855b5f73e8f5ea29bdf12cd9fe357be9f86e
babky <devnull@localhost>
2013-04-03 20:36:03 +0200

    Injection + validatory.
    #6019

--- asl/utils/injection_helper.py
+++ asl/utils/injection_helper.py
@@ -4,6 +4,7 @@ Created on 3.4.2013
 @author: Martin Babka
 '''
 from asl.application.service_application import service_application
+import injector
 
 _app = service_application
 
@@ -16,3 +17,26 @@ def instantiate(cls):
         task = cls()
 
     return task
+
+def inject(**bindings):
+    '''
+    Decorator for injecting parameters for ASL objects.
+    '''
+    def outer_wrapper(f):
+        '''
+        Just a convenience method - delegates everything to wrapper.
+        '''
+        def wrapper(*a, **kwargs):
+            '''
+            Properly installs the injector into the object so that the injection can be performed.
+            '''
+            inj = _app.get_injector()
+            # Install injector.
+            inj.install_into(a[0])
+            # Use the generic wrapper.
+            inject_f = injector.inject(**bindings)
+            # Call the method.
+            return inject_f(f)(*a, **kwargs)
+        return wrapper
+
+    return outer_wrapper
