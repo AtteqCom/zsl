0b839205a1634782c6e1e1d63ce5d6d83662a723
babky <devnull@localhost>
2013-05-02 14:24:24 +0200

    Nacitanie vendor.

--- asl/vendor/__init__.py
+++ asl/vendor/__init__.py
@@ -14,6 +14,9 @@ def do_init():
         if os.path.exists(p + '/vendor/'):
             path = os.path.abspath(p + '/vendor/')
             break
+        if os.path.exists(p + '/asl/vendor/'):
+            path = os.path.abspath(p + '/asl/vendor/')
+            break
 
     vendor_modules = ['flask_injector', 'injector', 'redis', 'sqlalchemy/sqlalchemy-rel_0_7/lib', 'bpython/bpython']
     for v in vendor_modules:
