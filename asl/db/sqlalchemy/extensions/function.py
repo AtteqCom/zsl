f88ee8370eaee02805f995456e13694fcd47bc42
JankoHrasko <devnull@localhost>
2013-08-01 17:31:16 +0200

    Mato Babka - pridanie novej funkcionality do asl

--- asl/db/sqlalchemy/extensions/function.py
+++ asl/db/sqlalchemy/extensions/function.py
@@ -5,20 +5,20 @@ Created on 1.8.2013
 '''
 from sqlalchemy.sql import expression
 from sqlalchemy.ext.compiler import compiles
-from sqlalchemy.types import Numeric
 
-def function(function_name):
+def function(function_name, data_type):
     class function(expression.FunctionElement):
-        type = Numeric()
+        pass
 
     def function_impl(element, compiler, **kw):
-        arg = list(element.clauses)
+        args = ", ".join(map(lambda x: compiler.process(x), list(element.clauses)))
         return "%s(%s)" % (
             function_name,
-            compiler.process(arg),
+            args
         )
 
     f = type('function_%s' % (function_name,), function.__bases__, dict(function.__dict__))
     f.name = function_name
+    f.type = data_type
     compiles(f)(function_impl)
     return f
