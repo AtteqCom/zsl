551b7a3a2158bbcf154722d3cbcbd87fab3530bb
Peter Morihladko <morihladko@atteq.com>
2013-08-22 09:38:50 +0200

    PHP helper

---
+++ asl/utils/php_helper.py
@@ -0,0 +1,13 @@
+'''
+Created on Aug 22, 2013
+
+@author: Peter Morihladko
+
+Some handy function when dealing with code written in PHP
+'''
+
+def bool_to_str(boolean):
+    '''
+    Convert ``boolean`` to string like PHP 
+    '''
+    return '1' if boolean else ''
