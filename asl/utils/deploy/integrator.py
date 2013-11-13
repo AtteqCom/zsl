afc8d27e0a22f654a2fbc186c0e2efb6dbd71e19
Peter Morihladko <morihladko@atteq.com>
2013-11-13 17:14:31 +0100

    #7287 - js model generator, integrator

---
+++ asl/utils/deploy/integrator.py
@@ -0,0 +1,44 @@
+import tempfile
+import os
+
+def integrate_to_file(what, filename, start_line, end_line):
+    """
+    Integrate content into a file withing "line marks"
+    """
+
+    try:
+        with open(filename) as f:
+            lines = f.readlines()
+    except IOError:
+        lines = []
+
+    tmp_file = tempfile.NamedTemporaryFile(delete=False)
+
+    lines.reverse()
+    
+    # first copy before start line
+    while lines:
+        line = lines.pop()
+
+        if line == start_line:
+            break
+
+        tmp_file.write(line)
+    
+    # insert content
+    tmp_file.write(start_line)
+    tmp_file.write(what)
+    tmp_file.write(end_line)
+    
+    # skip until end line
+    while lines:
+        line = lines.pop()
+
+        if line == end_line:
+            break
+    
+    # copy rest
+    tmp_file.writelines(lines)
+    tmp_file.close()
+
+    os.rename(tmp_file.name, filename)
