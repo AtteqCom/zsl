e9f5ce81eb8a555ddc34f526beccd53c7c10e122
Peter Morihladko <morihladko@atteq.com>
2014-06-05 15:45:31 +0200

    Exposer pre resource

--- asl/utils/resource_helper.py
+++ asl/utils/resource_helper.py
@@ -2,11 +2,11 @@ import importlib
 from flask import request
 from imp import reload
 
-from asl.application.service_application import service_application
+from asl.application.service_application import service_application as app
 from asl.utils.string_helper import underscore_to_camelcase
 from asl.utils.task_helper import instantiate
+from asl.resource.model_resource import ModelResource
 
-app = service_application
 resource_packages = app.config['RESOURCE_PACKAGES'] if 'RESOURCE_PACKAGES' in app.config else (app.config['RESOURCE_PACKAGE'],)
 
 class MethodNotImplementedException(Exception):
@@ -26,27 +26,34 @@ def get_method(resource, method):
 def get_resource_task(resource_path):
     class_name = underscore_to_camelcase(resource_path) + 'Resource'
 
-    module = None    
+    resource = None    
     for resource_package in resource_packages:
         module_name = "{0}.{1}".format(resource_package, resource_path)
-    
+        
         try:
             module = importlib.import_module(module_name)
+            
+            try:
+                cls = getattr(module, class_name)
+
+                resource = instantiate(cls)
+                break
+            except AttributeError:
+                raise NameError("Can't find resource [{0}]".format(resource_path))
+            
         except ImportError:
-            pass
-        
-    if module is None:
-        raise ImportError("No module named {0} found in [{1}]".format(module_name, ",".join(resource_packages)))
-
-    # TODO if app.is_debug
-    reload(module)
-
-    # Create the resource using the injector initialization.
-    try:
-        cls = getattr(module, class_name)
-    except AttributeError:
-        raise ImportError("cannot find [{0}] in module [{1}]".format(class_name, module_name))
-    resource = instantiate(cls)
+            
+            # try to find __exposer__
+            try:
+                exposer = importlib.import_module(resource_package + '.__exposer__')
+                
+                resource = exposer.get_resource(class_name)
+                break
+            except ImportError:
+                pass
+            
+    if resource is None:
+        raise NameError("Can't find resource [{0}]".format(resource_path))
 
     try:
         if request.method == 'POST':
@@ -67,3 +74,18 @@ def get_resource_task(resource_path):
     except MethodNotImplementedException:
         app.logger.debug("MethodNotImplementedException raised for method [%s] and path [%s]", request.method, resource_path)
         return None
+    
+def create_model_resource(resource_map, name):
+    '''
+    Create a model resource from a dict ``resource_map`` {'resource name': ('model package', 'model class')}
+    '''
+    try:
+        module_name, model_name = resource_map[name]
+    except KeyError:
+        raise ImportError()
+
+    module = importlib.import_module(module_name)
+    model_cls = getattr(module, model_name)
+    
+    return app.get_injector().create_object(ModelResource, {'model_cls': model_cls})    
+
