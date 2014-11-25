cb6fa12d27ca2aacaa794b0cd49d66a5dd7801d6
Martin Babka <devnull@localhost>
2014-11-25 14:42:31 +0100

    One todo removed. For the tasks - the method perform is no longer a literal constant but a well-defined constant.

--- asl/utils/task_helper.py
+++ asl/utils/task_helper.py
@@ -8,6 +8,8 @@ from asl.task.task_data import TaskData
 
 _app = service_application
 
+TASK_PERFORM_METHOD = "perform"
+
 def run_task(task_cls, task_data):
     task = instantiate(task_cls)
     task_callable = get_callable(task)
@@ -21,8 +23,7 @@ def run_task_json(task_cls, task_data):
     return task_callable(td)
 
 def get_callable(task):
-    # TODO: Make perform into a constant!
-    return getattr(task, "perform")
+    return getattr(task, TASK_PERFORM_METHOD)
 
 from injection_helper import instantiate
 
