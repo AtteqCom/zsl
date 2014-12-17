beeec517b5c51ceded7f1f1146bb0f6547defd2d
babky <devnull@localhost>
2014-12-17 15:44:11 +0100

    Better vendor path guess.

--- asl/vendor/__init__.py
+++ asl/vendor/__init__.py
@@ -9,14 +9,18 @@ def do_init():
         return
 
     _vendor_initialized = True
+
     path = "."
-    for p in sys.path:
-        if os.path.exists(p + '/vendor/'):
-            path = os.path.abspath(p + '/vendor/')
-            break
-        if os.path.exists(p + '/asl/vendor/'):
-            path = os.path.abspath(p + '/asl/vendor/')
-            break
+    if os.path.exists('./asl/asl/vendor'):
+        path = os.path.abspath('./asl/asl/vendor')
+    else:
+        for p in sys.path:
+            if os.path.exists(p + '/vendor/'):
+                path = os.path.abspath(p + '/vendor/')
+                break
+            if os.path.exists(p + '/asl/vendor/'):
+                path = os.path.abspath(p + '/asl/vendor/')
+                break
 
     vendor_modules = ['injector', 'flask_injector', 'redis-py', 'sqlalchemy/sqlalchemy-0_9_1/lib', 'bpython/bpython', 'sphinxapi']
     new_path = []
