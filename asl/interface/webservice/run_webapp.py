85724f862bc131fd008ee83e6868e0f42ad63ef0
babky <devnull@localhost>
2015-02-03 08:25:48 +0100

    Changes for starter - better PYTHONPATH handling.

--- asl/interface/webservice/run_webapp.py
+++ asl/interface/webservice/run_webapp.py
@@ -2,9 +2,8 @@
 
 import sys
 import os
-from asl.interface.run import run_webapp
-
 sys.path.append(os.path.join(os.path.dirname(__file__), '..', '..', '..'));
+from asl.interface.run import run_webapp
 
 if __name__ == "__main__":
     run_webapp()
