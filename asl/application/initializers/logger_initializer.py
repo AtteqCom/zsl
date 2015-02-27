8b559650e9efa8032687a7b371760f2500c39b93
Martin Babka <devnull@localhost>
2015-02-27 14:01:20 +0100

    Sexy logging settings.

--- asl/application/initializers/logger_initializer.py
+++ asl/application/initializers/logger_initializer.py
@@ -12,6 +12,32 @@ app = service_application
 
 class LoggerInitializer:
 
+    def _create_handler(self, handler_settings):
+        parameters = handler_settings['parameters'] if 'parameters' in handler_settings else {}
+        handler_type = handler_settings['type']
+
+        if handler_type == "syslog":
+            handler = SysLogHandler(**parameters)
+        elif handler_type == "file":
+            handler = logging.FileHandler(**parameters)
+        elif handler_type == "rotating-file":
+            handler = logging.handlers.RotatingFileHandler(**parameters)
+        elif getattr(logging, handler_type):
+            handler = getattr(logging, handler_type)(**parameters)
+        else:
+            raise Exception('Unknown logger type {0}.'.fromat(handler_settings['type']))
+
+        handler.setFormatter(Formatter('[%(asctime)s %(name)s %(filename)s:%(levelname)s] %(message)s'))
+        return handler
+
+    def _check_deprecated_config_properties(self, config):
+        deprecated_properties = ['LOG_HANDLER', 'LOG_LEVEL', 'SYSLOG_PARAMS', 'DATABASE_LOG_LEVEL']
+        log = logging.getLogger(app.logger_name)
+
+        for prop in deprecated_properties:
+            if prop in config:
+                log.warn("Config property {0} is deprecated and is ignored. Use the new logging setting.".format(prop))
+
     def initialize(self, binder):
         config = binder.injector.get(Config)
         binder.bind(
@@ -20,31 +46,29 @@ class LoggerInitializer:
             scope=singleton
         )
 
-        if 'LOG_HANDLER' not in config:
-            config['LOG_HANDLER'] = 'rotating-file'
-        if 'LOG_LEVEL' not in config:
-            config['LOG_LEVEL'] = 'DEBUG' if app.debug else 'WARNING'
-        else:
-            config['LOG_LEVEL'] = config['LOG_LEVEL']
+        errors = []
 
-        if config['LOG_HANDLER'] == "syslog":
-            handler = SysLogHandler(**config['SYSLOG_PARAMS'])
-        elif config['LOG_HANDLER'] == "file":
-            handler = logging.FileHandler(config['LOG_FILE'])
-        elif config['LOG_HANDLER'] == "rotating-file":
-            handler = logging.handlers.RotatingFileHandler(config['LOG_FILE'])
-        elif config['LOG_HANDLER'] == None or config['LOG_HANDLER'].lower() == 'none':
-            return
+        # Create the handlers.
+        handlers = {}
+        for (handler_name, handler_settings) in config.get('LOG_HANDLERS', {}).iteritems():
+            try:
+                handlers[handler_name] = self._create_handler(handler_settings)
+            except Exception as e:
+                errors.append(e)
 
-        handler.setLevel(getattr(logging, config['LOG_LEVEL']))
-        handler.setFormatter(Formatter('[%(asctime)s] [%(name)s-%(levelname)s] [%(filename)s] %(message)s'))
+        # Set up the loggers.
+        for (logger_name, logger_settings) in config.get('LOG', {}).iteritems():
+            logger = logging.getLogger(logger_name)
+            logger.setLevel(getattr(logging, logger_settings['level']))
+            for handler_name in logger_settings['handlers']:
+                try:
+                    logger.addHandler(handlers[handler_name])
+                except Exception as e:
+                    errors.append(e)
 
-        logging.getLogger(app.logger_name).addHandler(handler)
-        logging.getLogger(app.logger_name).setLevel(getattr(logging, config['LOG_LEVEL']))
-
-        db_logger = logging.getLogger('sqlalchemy.engine')
-        db_logger.setLevel(config['DATABASE_LOG_LEVEL'])
-        db_logger.addHandler(handler)
+        self._check_deprecated_config_properties(config)
+        for e in errors:
+            app.logger.error(e)
 
 @injection_module
 def logger_initializer_module(binder):
