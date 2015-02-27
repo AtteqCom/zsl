7ceec92c64fa957ab0ee35c4521d0cc9e4406d6b
babky <devnull@localhost>
2015-02-27 11:55:05 +0100

    External libraries initializer.

--- asl/vendor/__init__.py
+++ asl/vendor/__init__.py
@@ -3,6 +3,12 @@ import os
 
 _vendor_initialized = False
 
+def append_paths(path, vendor_modules):
+    new_path = []
+    for v in vendor_modules:
+        new_path.append(path + os.sep + v)
+    sys.path = new_path + sys.path
+
 def do_init():
     global _vendor_initialized
     if _vendor_initialized:
@@ -23,9 +29,6 @@ def do_init():
                 break
 
     vendor_modules = ['injector', 'flask_injector', 'redis-py', 'sqlalchemy/sqlalchemy-0_9_1/lib', 'bpython/bpython', 'sphinxapi', 'simplejson']
-    new_path = []
-    for v in vendor_modules:
-        new_path.append(path + os.sep + v)
-    sys.path = new_path + sys.path
+    append_paths(path, vendor_modules)
 
 do_init()
