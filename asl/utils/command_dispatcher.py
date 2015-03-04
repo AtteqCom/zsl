99af03ef01ef236969411f39f2db90ed00556db7
Peter Morihladko <morihladko@atteq.com>
2015-03-04 22:45:50 +0100

    Command dispatcher

---
+++ asl/utils/command_dispatcher.py
@@ -0,0 +1,83 @@
+"""
+Created on Mar 4, 2015
+
+@author: peter morihladko
+"""
+
+import inspect
+
+
+class CommandDispatcher(object):
+    """
+    A simple class for command dictionary. A command is a function
+    which can take named parameters.
+    """
+
+    def __init__(self):
+        """
+        Create command dictionary
+        """
+
+        self.commands = {}
+        """command dictionary"""
+
+        self._required_arguments = {}
+
+    def command(self, fn):
+        """
+        Add method or function to dispatcher. Can be use as a nice
+        decorator.
+
+        :param fn: function or method
+        :type fn: function
+        :return: the same function
+        :rtype: function
+        """
+        self.commands[fn.__name__] = fn
+
+        return fn
+
+    """alias for ``CommandDispatcher.command``"""
+    add_function = command
+
+    def execute_command(self, command, args=None):
+        """
+        Execute a command
+
+        :param command: name of the command
+        :type command: str
+        :param args: optional named arguments for command
+        :type args: dict
+        :return: the result of command
+        :raises KeyError: if command is not found
+        """
+
+        if args in None:
+            args = {}
+
+        command_fn = self.commands[command]
+
+        return command_fn(**args)
+
+    def bound(self, instance):
+        """
+        Return a new dispatcher, which will switch all command functions
+        with bounded methods of given instance matched by name. It will
+        match only regular methods.
+
+        :param instance: object instance
+        :type instance: object
+        :return: new Dispatcher
+        :rtype: CommandDispatcher
+        """
+
+        bounded_dispatcher = CommandDispatcher()
+        bounded_dispatcher.commands = self.commands.copy()
+
+        for name, fn in self.commands.iteritems():
+            method = getattr(instance, name, None)
+
+            if method and inspect.ismethod(method) and method.__self__ == instance:
+                bounded_dispatcher.commands[name] = method
+
+        return bounded_dispatcher
---
+++ tests/utils/test_command_dispatcher.py
@@ -0,0 +1,135 @@
+import unittest
+import types
+from asl.utils.command_dispatcher import CommandDispatcher
+
+__author__ = 'peter'
+
+
+class TestCommandDispatcher(unittest.TestCase):
+    def setUp(self):
+        self.dispatcher = CommandDispatcher()
+
+    def test_add_function(self):
+
+        def dummy_function():
+            pass
+
+        self.assertEqual(len(self.dispatcher.commands), 0, "Empty commands")
+
+        self.dispatcher.add_function(dummy_function)
+
+        self.assertEqual(len(self.dispatcher.commands), 1, "Command added")
+        self.assertEqual(self.dispatcher.commands[dummy_function.__name__],
+                         dummy_function,
+                         "Command added")
+
+    def test_command_decorator(self):
+
+        def another_dummy_fn():
+            pass
+
+        decorated_fn = self.dispatcher.command(another_dummy_fn)
+
+        self.assertEqual(decorated_fn,
+                         another_dummy_fn,
+                         "Decorator hasn't returned the same function")
+
+        @self.dispatcher.command
+        def dummy_function():
+            pass
+
+        self.assertEqual(self.dispatcher.commands[dummy_function.__name__],
+                         dummy_function,
+                         "Decorator didn't decorate")
+
+    @property
+    def test_command_execute(self):
+        dispatcher = self.dispatcher
+
+        @dispatcher.command
+        def return_five():
+            return 5
+
+        @dispatcher.command
+        def adder(a, b):
+            return a + b
+
+        @dispatcher.command
+        def summer(n, **kwargs):
+            return sum(map(lambda x: x*n, kwargs.values()))
+
+        self.assertEqual(dispatcher.execute_command('return_five'),
+                         5, "Command didn't execute properly")
+
+        self.assertEqual(dispatcher.execute_command('adder', {'a': 1, 'b': 2}),
+                         adder(1, 2),
+                         "Command has not taken given arguments")
+
+        # testparameters
+        self.assertRaises(TypeError,
+                          dispatcher.execute_command, 'adder', {'a': 1, 'b': 2, 'e': 3})
+        self.assertRaises(TypeError, dispatcher.execute_command, 'adder')
+
+        self.assertEquals(dispatcher.execute_command('summer', {'n': 2, '1': 1, '2': 2, '3': 3}),
+                          sum([2, 4, 6]),
+                          "Command doesn't support kwarg parameters")
+
+        self.assertRaises(KeyError,
+                          dispatcher.execute_command('error_command'))
+
+    def test_bound(self):
+        dispatcher = self.dispatcher
+
+        class C(object):
+            def __init__(self, a, b):
+                self._a = a
+                self._b = b
+
+            @dispatcher.command
+            def get_sum(self, x, y):
+                return self._a + self._b + x + y
+
+        class D(object):
+            def __init__(self, a, b):
+                self._a = a
+                self._b = b
+
+            @dispatcher.command
+            def get_other_sum(self, x, y):
+                return self._a + self._b + x + y
+
+        @dispatcher.command
+        def dummy_fun():
+            return 5
+
+        c = C(1, 2)
+        d = D(10, 20)
+
+        bounded_dispatcher = dispatcher.bound(c)
+
+        self.assertEqual(bounded_dispatcher.commands['get_sum'], c.get_sum,
+                         "Bound has not bounded the method")
+        self.assertEqual(bounded_dispatcher.execute_command('get_sum', {'x': 10, 'y': 200}),
+                         c.get_sum(10, 200),
+                         "Bound method has false result")
+
+        self.assertIs(type(bounded_dispatcher.commands['get_other_sum']), types.FunctionType,
+                      "Bound has bounded a foreign method")
+        self.assertEqual(bounded_dispatcher.execute_command('get_other_sum', {'self': d, 'x': 1000, 'y': 300}),
+                         d.get_other_sum(1000, 300),
+                         "Bound has returned a false result for not bounded method")
+
+        self.assertEqual(bounded_dispatcher.execute_command('dummy_fun'),
+                         5, "Bound has returned a false result for normal function")
+
+
+
+
+
+
+
+
+
+
+
+
