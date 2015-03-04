71a572ff3088bba10caadec7ead6626339538361
Peter Morihladko <morihladko@atteq.com>
2015-03-04 22:46:56 +0100

    Added safe_args for checking func arguments

--- asl/utils/params_helper.py
+++ asl/utils/params_helper.py
@@ -1,20 +1,57 @@
-'''
-Modul na ulahcenie prace s parametrami 
+"""
+Helpers for parameter handling
 
 @author Peter Morihladko
-'''
+"""
+
+import inspect
+
 
 class RequestException(Exception):
     pass
 
-def required_params(data, *required_params):
-    '''
-    Skontroluje ci dane parametre sa nachadzaju v data parametre,
-    ak nie vyhodi vynimku
-    '''
-    
-    if not reduce(lambda still_valid, param: still_valid and param in data, required_params, True):
-        raise RequestException(msg_err_missing_params(*required_params)) 
+
+def required_params(data, *r_params):
+    """
+    Check if given parameters are in the given dict, if not raise
+    an exception
+
+    :param data: data to check
+    :type data: dict
+    :param required_params: required parameters
+    :raise RequestException: if params not in data
+    """
+
+    if not reduce(lambda still_valid, param: still_valid and param in data,
+                  r_params, True):
+        raise RequestException(msg_err_missing_params(*r_params))
+
 
 def msg_err_missing_params(*params):
     return "Missing one or more required parameters (%s)" % '|'.join(params)
+
+
+def safe_args(fn, args):
+    """
+    Check if ``args`` as a dictionary has the required parameters of
+    ``fn`` function and filter any waste parameters so ``fn`` can be
+    safely called with them
+
+    :param fn: function object
+    :type fn: callable
+    :param args: dictionary of parameters
+    :type args: dict
+    :return: dictionary to be used as named params for the ``fn``
+    :rtype: dict
+    '"""
+    fn_args = inspect.getargspec(fn)
+
+    if fn_args.defaults:
+        required_params(args, fn_args.args[:-len(fn_args.defaults)])
+    else:
+        required_params(args, fn_args)
+
+    if not fn_args.keywords:
+        return dict((key, value) for key, value in args.iteritems() if key in fn_args.args)
+    else:
+        return args
