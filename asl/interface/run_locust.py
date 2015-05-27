ea1405569256687b2f47f59e20c8021c8cec538e
Peter Morihladko <morihladko@atteq.com>
2015-05-27 17:45:52 +0200

    run_locust added

---
+++ asl/interface/run_locust.py
@@ -0,0 +1,14 @@
+# EASY-INSTALL-ENTRY-SCRIPT: 'locustio==0.7.2','console_scripts','locust'
+__requires__ = 'locustio==0.7.2'
+from importer import initialize_cli_application
+initialize_cli_application()
+
+import sys
+from pkg_resources import load_entry_point
+
+
+if __name__ == '__main__':
+    sys.exit(
+        load_entry_point('locustio==0.7.2', 'console_scripts', 'locust')()
+    )
+
