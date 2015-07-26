9910d9a144a98aed7b4cb763c02af1b3e34a7d5c
lubbo <devnull@localhost>
2015-07-26 10:56:53 +0200

    Bug #10166: Nezobrazujú sa fotky športovcov v boxiku - refactor.

--- asl/utils/url_helper.py
+++ asl/utils/url_helper.py
@@ -18,10 +18,3 @@ def slugify(value):
     value = unicodedata.normalize('NFKD', value).encode('ascii', 'ignore')
     value = unicode(re.sub('[^\w\s-]', '', value).strip().lower())
     return re.sub('[-\s]+', '-', value)
-
-
-def club(club):
-    return '/sportove-kluby/{id}/{name}'.format(id=club.id, name=slugify(club.name))
-
-def image(image, dim):
-    return '/cacheImg/obr/{0}px/{1}-{2}.{3}'.format(dim, slugify(image.description), image.iid, image.extension)
