342574f954a7f4829b5c365e91c1c8ddea7c21a7
Martin Babka <babka@atteq.com>
2016-05-05 09:22:18 +0200

    Fix - gearman caller.

--- asl/utils/gearman_helper.py
+++ asl/utils/gearman_helper.py
@@ -3,4 +3,4 @@ import json
 
 def schedule_german_task(app, path, data):
     gm_client = gearman.GearmanClient(["{0}:{1}".format(app.config['GEARMAN']['host'], app.config['GEARMAN']['port'])])
-    gm_client.submit_job(app.config['GEARMAN_TASK_NAME'], json.dumps({ 'path': path, 'data': data }), wait_until_complete=False, background=True)
+    gm_client.submit_job(app.config['GEARMAN_TASK_NAME'], json.dumps({ 'path': path, 'data': json.dumps(data) }), wait_until_complete=False, background=True)
