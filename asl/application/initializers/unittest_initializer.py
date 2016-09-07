0bfc59983287b7d9650d7af52fa2187cdd203868
Martin Babka <babka@atteq.com>
2016-09-07 14:36:44 +0200

    Improved the unit testing.

--- asl/application/initializers/unittest_initializer.py
+++ asl/application/initializers/unittest_initializer.py
@@ -12,8 +12,12 @@ class UnitTestInitializer(object):
         if not self.is_unit_testing():
             return
 
-        service_application.config['DATABASE_URI'] = service_application.config['TEST_DATABASE_URI']
-        service_application.config['DATABASE_ENGINE_PROPS'] = service_application.config['TEST_DATABASE_ENGINE_PROPS']
+        if 'TEST_DATABASE_URI' in service_application.config:
+            service_application.config['DATABASE_URI'] = service_application.config['TEST_DATABASE_URI']
+
+        if 'TEST_DATABASE_ENGINE_PROPS' in service_application.config:
+            service_application.config['DATABASE_ENGINE_PROPS'] = service_application.config[
+                'TEST_DATABASE_ENGINE_PROPS']
 
     def is_unit_testing(self):
         return service_application.get_initialization_context().unit_testing
