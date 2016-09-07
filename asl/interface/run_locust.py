499930d3db20af1c7a734f3c454d67194e76b048
Martin Babka <babka@atteq.com>
2016-09-07 14:52:10 +0200

    Fixed initialization for locust.

--- asl/interface/run_locust.py
+++ asl/interface/run_locust.py
@@ -1,7 +1,7 @@
 # EASY-INSTALL-ENTRY-SCRIPT: 'locustio==0.7.2','console_scripts','locust'
-from asl.interface.importer import InitializationContext
 __requires__ = 'locustio==0.7.2'
-from importer import initialize_cli_application
+
+from importer import initialize_cli_application, InitializationContext
 initialize_cli_application(InitializationContext(unit_test=False))
 
 import sys
