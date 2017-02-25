866e4411e8d238a5178bb292a98e5f9d99ad53a3
Peter Morihladko <morihladko@gmail.com>
2017-02-25 02:08:19 +0100

    Remove the celery app from the CeleryWorkerBase
    After reviewing the code and some though I concluded that it will be
    more flexible to remove celery app initialization from the
    CeleryTaskQueueWorkerBase and CeleryTaskQueueOutsideWorker.
    
    The app can be initialized from the client and can be integrated into
    existing celery projects more easly.

--- zsl/interface/celery/worker.py
+++ zsl/interface/celery/worker.py
@@ -13,7 +13,6 @@ import sys
 from celery import Celery, shared_task
 from injector import Module, singleton, provides
 
-from zsl import Config
 from zsl.utils.injection_helper import inject
 from zsl.interface.task_queue import TaskQueueWorker
 from zsl.task.job_context import Job
@@ -22,16 +21,8 @@ from zsl.task.job_context import Job
 class CeleryTaskQueueWorkerBase(TaskQueueWorker):
     """Base class for celery task queue worker.
 
-    It initialize the celery app in constructor.
+    It contains only the task execution part of worker.
     """
-
-    @inject(config=Config)
-    def __init__(self, config):
-        # type: (Config) -> None
-        super(CeleryTaskQueueWorkerBase, self).__init__()
-
-        self.celery_app = Celery()
-        self.celery_app.config_from_object(config['CELERY'])
 
     def execute_celery_task(self, job_data):
         # type: (dict) -> dict
@@ -61,11 +52,13 @@ class CeleryTaskQueueOutsideWorker(CeleryTaskQueueWorkerBase):
 
 
 class CeleryTaskQueueMainWorker(CeleryTaskQueueWorkerBase):
-    """Worker implementation for Celery task queue.
-    """
-    def __init__(self):
+    """Worker implementation for Celery task queue."""
+
+    def __init__(self,):
         super(CeleryTaskQueueMainWorker, self).__init__()
 
+        self.celery_app = Celery()
+        self.celery_app.config_from_object(self._config['CELERY'])
         self.celery_worker = None
 
     def stop_worker(self):
