1f74840f9b8cd1705dc622cc9e20c933b2853543
Peter Morihladko <morihladko@gmail.com>
2017-05-10 23:42:37 +0200

    Add pypi deploy settings to travis

--- setup.py
+++ setup.py
@@ -3,21 +3,20 @@ from setuptools import setup, find_packages
 EXCLUDE_FROM_PACKAGES = ['tests', 'tests.*']
 
 setup(name='zsl',
-      version='0.11.0',
+      version='0.12.0',
       description='zsl application framework for web based services',
       author='Atteq s.r.o.',
       author_email='open.source@atteq.com',
       url='https://github.com/AtteqCom/zsl',
       license='MIT',
       install_requires=[
-        'injector',
-        'flask',
-        'flask_injector',
+        'flask_injector==0.8.0',
         'future'
       ],
       classifiers=[
           'Development Status :: 3 - Alpha',
           'Programming Language :: Python :: 2.7',
+          'Programming Language :: Python :: 3.6',
           'Operating System :: OS Independent',
           'Topic :: Software Development :: Libraries :: Application Frameworks'
       ],
