cbffeea20f762eac754b3e8aeacb1a7b408770af
Martin Babka <babka@atteq.com>
2017-05-22 17:11:54 +0200

    Fix requested changes from PR - add back compatibility support

--- zsl/interface/webservice/utils/request_data.py
+++ zsl/interface/webservice/utils/request_data.py
@@ -1,3 +1,8 @@
+from __future__ import (absolute_import, division,
+                        print_function, unicode_literals)
+from builtins import *
+
+
 def extract_data(request):
     data = request.form.to_dict(flat=True)
     if request.json:
