8df7eecc4c403a5ca893f8b8e58d2d3f342288d3
Martin Babka <babka@atteq.com>
2017-06-07 20:13:23 +0200

    Improve nested_models function field specification
    The field specification may now be now only string but a raw model field
    declaration from the raw model class.

--- zsl/db/helpers/nested.py
+++ zsl/db/helpers/nested.py
@@ -2,18 +2,32 @@
 :mod:`zsl.db.helpers.nested`
 ----------------------------
 
-.. moduleauthor:: peter
+.. moduleauthor:: Peter Morihladko <morihladko@atteq.com>
 """
 from __future__ import unicode_literals
+from builtins import *
+from typing import Any
+from typing import Optional
+
+from sqlalchemy.orm.attributes import InstrumentedAttribute
+
+from zsl.db.model.app_model import AppModel
+from zsl.db.model.raw_model import ModelBase
+
+
+def get_nested_field_name(field):
+    if isinstance(field, InstrumentedAttribute):
+        field = field.property.key
+
+    return field
 
 
 def nested_model(model, nested_fields):
+    # type: (ModelBase, Any)->Optional[AppModel]
+    """Return :class:`zsl.db.model.app_model import AppModel` with the nested 
+    models attached. ``nested_fields`` can be a simple list as model fields, 
+    or it can be a tree definition in dict with leafs as keys with None value
     """
-    Return app_model with nested models attached. ``nested_fields`` can
-    be a simple list as model fields, or it can be a tree definition
-    in dict with leafs as keys with None value
-    """
-
     if model is None:
         return None
 
@@ -21,7 +35,10 @@ def nested_model(model, nested_fields):
     is_dict = isinstance(nested_fields, dict)
 
     for field in nested_fields:
-        nested_nested = nested_fields.get(field) if is_dict and nested_fields.get(field) else []
+        field = get_nested_field_name(field)
+
+        nested_nested = nested_fields.get(
+            field) if is_dict and nested_fields.get(field) else []
         value = getattr(model, field, None)
 
         # we can have also lists in field
@@ -33,7 +50,7 @@ def nested_model(model, nested_fields):
 
 
 def nested_models(models, nested_fields):
-    """
-    For a list of ``models`` apply ``get_nested_model`` with given ``nested_fields``
+    """For a list of ``models`` apply ``get_nested_model`` with given 
+    ``nested_fields``.
     """
     return [nested_model(model, nested_fields) for model in models]
