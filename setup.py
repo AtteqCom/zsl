dbef37c4df72347fefbf0ca6a55871c9d2fd5f8a
Martin Babka <babka@atteq.com>
2017-06-22 17:28:51 +0200

    Increment version after larger changeset
    1. Flask injector dependency is removed.
    2. Models of requests and responses are supported.

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ if sys.version_info < (3, 0):
     requirements.append('enum34')
 
 setup(name='zsl',
-      version='0.15.9',
+      version='0.16.0',
       description='zsl application framework for web based services',
       author='Atteq s.r.o.',
       author_email='open.source@atteq.com',
