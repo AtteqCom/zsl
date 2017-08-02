bd4067d253e74cade378f2fab840556107c0e323
Martin Babka <babka@atteq.com>
2017-08-02 22:11:49 +0200

    Refactor pagination

--- src/zsl/db/helpers/pagination.py
+++ src/zsl/db/helpers/pagination.py
@@ -6,39 +6,55 @@ from __future__ import unicode_literals
 from builtins import object
 
 from sqlalchemy.orm.query import Query
+from zsl.db.model.app_model import AppModel
 
 FIRST_PAGE = 1
-DEFAULT_PAGE_RECORD_COUNT = 25
+DEFAULT_PAGE_SIZE = 25
 
 
 class Pagination(object):
+    """
+    Pagination support. Allows to paginate a query. There are two choices.
+
+        1. :meth:`.paginate` - paginates a query and obtains the count
+            automatically.
+        1. :meth:`.apply_pagination` - paginates a query and assumes that
+            record count is set in advance.
+    """
+
     def __init__(self, pagination=None):
         pagination = self._create_pagination_model(pagination)
-        assert isinstance(pagination, PaginationModel)
-        self._offset = (pagination.page_no - FIRST_PAGE) * pagination.page_record_count
-        self._page_record_count = pagination.page_record_count
-        self._record_count = 0
+        assert isinstance(pagination, PaginationRequest)
+        self._offset = (pagination.page_no - FIRST_PAGE) * pagination.page_size
+        self._page_size = pagination.page_size
+        self._record_count = -1
 
     def _create_pagination_model(self, pagination):
         if pagination is None:
-            pagination = PaginationModel()
+            pagination = PaginationRequest()
         elif isinstance(pagination, dict):
-            page_record_count = int(pagination.get('page_record_count', DEFAULT_PAGE_RECORD_COUNT))
-            pagination = PaginationModel(FIRST_PAGE + int(pagination.get('offset', 0)) // page_record_count,page_record_count)
+            page_size = int(pagination.get('page_size', DEFAULT_PAGE_SIZE))
+            pagination = PaginationRequest(
+                FIRST_PAGE + int(pagination.get('offset', 0)) // page_size,
+                page_size)
         return pagination
 
-    def set_record_count(self, record_count):
-        self._record_count = record_count
-
-    def get_record_count(self):
+    @property
+    def record_count(self):
         return self._record_count
 
-    def get_page_record_count(self):
-        return self._page_record_count
+    @record_count.setter
+    def record_count(self, record_count):
+        self._record_count = record_count
 
-    def get_offset(self):
-        count = self.get_record_count()
-        per_page = self.get_page_record_count()
+    @property
+    def page_size(self):
+        return self._page_size
+
+    @property
+    def offset(self):
+        count = self._record_count
+        per_page = self.page_size
         if self._offset >= count:
             last_page_size = count % per_page
             if last_page_size == 0:
@@ -50,17 +66,40 @@ class Pagination(object):
         return self._offset
 
     def apply_pagination(self, q):
+        """
+        Filters the query so that a given page is returned. The record count
+        must be set in advance.
+        :param q: Query to be paged.
+        :return: Paged query.
+        """
         # type: (Query)->Query
-        return q.limit(self.get_page_record_count()).offset(self.get_offset())
+        assert self.record_count >= 0, "Record count must be set."
+        return q.limit(self.page_size).offset(self.offset)
 
     def paginate(self, q):
+        """
+        Filters the query so that a given page is returned. The record count
+        is computed automatically from query.
+        :param q: Query to be paged.
+        :return: Paged query.
+        """
         self.set_record_count(q.count())
         return self.apply_pagination(q).all()
 
 
-class PaginationModel(object):
+class PaginationRequest(AppModel):
     def __init__(self, page_no=FIRST_PAGE,
-                 page_record_count=DEFAULT_PAGE_RECORD_COUNT):
-        self.page_no = int(page_no if page_no else FIRST_PAGE)
-        self.page_record_count = int(
-            page_record_count if page_record_count else DEFAULT_PAGE_RECORD_COUNT)
+                 page_size=DEFAULT_PAGE_SIZE):
+        super(PaginationRequest, self).__init__({})
+        self.page_no = int(page_no) if page_no else FIRST_PAGE
+        self.page_size = int(page_size) if page_size else DEFAULT_PAGE_SIZE
+
+
+class PaginationResponse(AppModel):
+    def __init__(self, record_count: int, page_size: int,
+                 pagination: PaginationRequest) -> None:
+        super(PaginationResponse, self).__init__({})
+        self.record_count = record_count
+        max_page_size = pagination.page_size
+        self.page_count = (record_count + max_page_size - 1) // max_page_size
+        self.page_size = page_size
