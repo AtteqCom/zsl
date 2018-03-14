4e51329a4818dc8387f4f9d39764c397c7d78ba8
Martin Babka <babkys@gmail.com>
2018-03-14 22:23:46 +0100

    Bump version: 0.19.5 → 0.19.6

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ if sys.version_info < (3, 0):
     requirements.append('enum34')
 
 setup(name='zsl',
-      version='0.19.5',
+      version='0.19.6',
       description='zsl application framework for web based services',
       author='Atteq s.r.o.',
       author_email='open.source@atteq.com',
