8d0da8fec897b00e948a5572daa876f89569b134
Martin Babka <babkys@gmail.com>
2019-03-20 20:00:40 +0100

    Reformat, add more tests and fix comments from PR

--- src/zsl/utils/dict_to_object_conversion.py
+++ src/zsl/utils/dict_to_object_conversion.py
@@ -1,12 +1,16 @@
 from __future__ import unicode_literals
 
-# noinspection PyCompatibility
-from builtins import int, str
 from datetime import date, datetime
 
+# noinspection PyCompatibility
+from builtins import int, str
 from future.utils import viewitems
+from typing import Any
+from typing import Dict
+from typing import Union
 
-from zsl.utils.date_helper import format_date_portable, format_datetime_portable
+from zsl.utils.date_helper import format_date_portable, \
+    format_datetime_portable
 
 DATE_DATA = 'date_data'
 DATETIME_DATA = 'datetime_data'
@@ -21,10 +25,13 @@ def extend_object_by_dict(target, dict_data, hints=None):
     for field_name, field_value in viewitems(dict_data):
         if isinstance(field_value, (type(None), str, int, float, bool)):
             if field_name in hints[DATE_DATA] and field_value is not None:
-                d = datetime.strptime(field_value, hints[DATE_DATA][field_name]).date()
+                d = datetime.strptime(field_value,
+                                      hints[DATE_DATA][field_name]).date()
                 setattr(target, field_name, format_date_portable(d))
-            elif field_name in hints[DATETIME_DATA] and field_value is not None:
-                d = datetime.strptime(field_value, hints[DATETIME_DATA][field_name])
+            elif field_name in hints[DATETIME_DATA] and \
+                    field_value is not None:
+                d = datetime.strptime(field_value,
+                                      hints[DATETIME_DATA][field_name])
                 setattr(target, field_name, format_datetime_portable(d))
             else:
                 setattr(target, field_name, field_value)
@@ -33,14 +40,32 @@ def extend_object_by_dict(target, dict_data, hints=None):
         elif isinstance(field_value, date):
             setattr(target, field_name, format_date_portable(field_value))
         elif field_name in hints[RELATED_FIELDS]:
-            related_cls = hints[RELATED_FIELDS][field_name][RELATED_FIELDS_CLASS]
-            related_hints = hints[RELATED_FIELDS][field_name].get(RELATED_FIELDS_HINTS)
+            related_cls = hints[RELATED_FIELDS][field_name][
+                RELATED_FIELDS_CLASS]
+            related_hints = hints[RELATED_FIELDS][field_name].get(
+                RELATED_FIELDS_HINTS)
             if isinstance(field_value, (list, tuple)):
-                setattr(target, field_name, [related_cls(x, 'id', related_hints) for x in field_value])
+                setattr(
+                    target,
+                    field_name,
+                    [
+                        related_cls(_to_dict(x), 'id', related_hints)
+                        for x in field_value
+                    ]
+                )
             else:
-                setattr(target, field_name, related_cls(field_value.__dict__, 'id', related_hints))
+                setattr(
+                    target,
+                    field_name,
+                    related_cls(_to_dict(field_value), 'id', related_hints)
+                )
         elif isinstance(field_value, (list, tuple)):
             setattr(target, field_name, [x for x in field_value])
+
+
+def _to_dict(val):
+    # type: (Union[Dict[str, Any], object]) -> Dict[str, Any]
+    return val if isinstance(val, dict) else val.__dict__
 
 
 def _get_hints(original_hints):
