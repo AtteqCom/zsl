74521808ff70524c1c9160187924fd1447ccc6bd
Martin Babka <babkys@gmail.com>
2019-11-10 10:37:37 +0100

    Add VSCode config

---
+++ .devcontainer/install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+set -e
+
+bash -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" || /bin/true
+curl https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/templates/zshrc.zsh-template > .zshrc
+echo "source ~/.bash_aliases" >> ~/.zshrc
+
+/usr/bin/python3.8 -m venv .venv 
+.venv/bin/pip install -r requirements.txt
