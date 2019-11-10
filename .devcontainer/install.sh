086f39cdb4958d47bdddfe99ef484b02437f93ab
Martin Babka <babka@atteq.com>
2019-11-10 19:05:51 +0000

    Install .zshrc to right location

--- .devcontainer/install.sh
+++ .devcontainer/install.sh
@@ -3,7 +3,7 @@
 set -e
 
 bash -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" || /bin/true
-curl https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/templates/zshrc.zsh-template > .zshrc
+curl https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/templates/zshrc.zsh-template > ~/.zshrc
 echo "source ~/.bash_aliases" >> ~/.zshrc
 
 /usr/bin/python3.8 -m venv .venv 
